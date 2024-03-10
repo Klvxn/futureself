138c92c7f624eb666be2a29414a86083a85f79cd
Klvn <akpulukelvin@gmail.com>
2024-03-10 22:44:32 +0100

    defined new actions to letters in admin

--- letter/admin.py
+++ letter/admin.py
@@ -5,7 +5,22 @@ from .models import Letter
 # Register your models here.
 @admin.register(Letter)
 class LetterAdmin(admin.ModelAdmin):
-    list_display = ('user', 'title', 'email_address', 'delivered')
+    list_display = ('user', 'title', 'email_address', 'delivered', 'audience')
     list_filter = ('audience',)
+    actions = ['mark_delivered', 'set_audience_public', 'set_audience_private']
 
+    @admin.action(description='Mark as delivered')
+    def mark_delivered(self, request, queryset):
+        queryset.update(delivered=True)
+        self.message_user(request, 'Marked as delivered')
 
+    @admin.action(description='Set audience public')
+    def set_audience_public(self, request, queryset):
+        queryset.update(audience='public, but as anon')
+        self.message_user(request, 'Marked as public') 
+    
+    @admin.action(description='Set audience private')
+    def set_audience_private(self, request, queryset):
+        queryset.update(audience='private')
+        self.message_user(request, 'Marked as private')    
+        

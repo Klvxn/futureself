f57f5ef7cfcd858deb21c2d6e9fedcf1795f5e54
Klvn <akpulukelvin@gmail.com>
2024-03-20 09:07:34 +0100

    add like fxn to url

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogEditView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogEditView, update_like
 
 
 app_name = 'blog'
@@ -8,5 +8,6 @@ urlpatterns = [
     path('all/', BlogListView.as_view(), name='blog_list'),
     path('<slug:slug>/<int:year>/<int:month>/', BlogDetailView.as_view(), name='blog_detail'),
     path('create/', BlogCreateView.as_view(), name='blog_create'),
+    path('like/<slug:slug>/', update_like, name='update_like'),
     path('<slug:slug>/<int:year>/<int:month>/edit/', BlogEditView.as_view(), name='blog_edit'),
 ]
